f9bfe95a909f1e23e27f745fe986d97b3eeacc2f
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-07-13 16:49:05 +0530

    Server setup

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+const express = require('express');
+const app = express()
+
+const port = process.env.PORT || 8080;
+
+app.listen(port,()=>console.log(`Server running on port: ${port}`));
