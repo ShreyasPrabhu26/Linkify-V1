ca9326571a69668cbd2e73644ddb47388cd88877
Shreyas Prabhu <84027050+ShreyasPrabhu26@users.noreply.github.com>
2024-07-29 12:31:20 +0530

    Optimize index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const { authLoggedInUser, checkAuth, restrictTo } = require('./middlewares/auth');
-const app = express();
 const favicon = require('serve-favicon');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const app = express();
 
 // Get data from environment variables
 const PORT = process.env.PORT || 8080;
@@ -19,14 +22,15 @@ const apiRouter = require("./routes/apiRoutes");
 
 // Middleware configuration
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(compression());
 app.use(checkAuth);
 app.use(favicon(path.join(__dirname, 'public/images/logo', 'favicon.png')));
-
 
 // Define your routes
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -35,7 +39,11 @@ app.use("/user", userRouter);
 app.use("/url", restrictTo(["ADMIN", "NORMAL"]), urlRouter);
 app.use("/api", apiRouter);
 
-
-connectToMongoDb(CONNECTION_URL);
-
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+connectToMongoDb(CONNECTION_URL)
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB', err);
+        process.exit(1);
+    });
